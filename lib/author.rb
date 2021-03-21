f823ce56b5631684efd2cb7890f75d8d581a60fb
Alexia <alexheg44@gmail.com>
2021-03-20 23:37:57 -0400

    commit tree d039785095d3ef8e5b92cf85cea7b586812a3fc1

---
+++ lib/author.rb
@@ -0,0 +1,6 @@
+class Author 
+    attr_accessor :name
+    def initiliaze(name)
+        @name = name
+    end
+end
---
+++ lib/post.rb
@@ -0,0 +1,7 @@
+class Post
+    attr_accessor :author, :title
+    def initiliaze(author,title)
+        @author = author
+        @title = title
+    end
+end
